5792b640724adcb3f707abd0924bb6e8844f3523
John Hillery <John@localhost>
2021-11-27 09:30:04 -0500

    Check globals first

--- src/NetBenefits.py
+++ src/NetBenefits.py
@@ -1,27 +1,26 @@
 # get Fidelity NetBenefits current balances
 from locale import LC_ALL, setlocale
 
+from java.lang import System
+
 from FwLookupWindow import FwLookupWindow
 from NbControl import NbControl
+from NbImporter import NbImporter
 
 if __name__ == "__main__":
     # pick up user's default locale
     setlocale(LC_ALL, "")
 
-    lookupWin = FwLookupWindow()
-    nbCtrl = NbControl(lookupWin)
-    lookupWin.visible = True
+    if 'moneydance' in globals():
+        lookupWin = FwLookupWindow()
+        nbCtrl = NbControl(lookupWin)
+        lookupWin.visible = True
 
-    if nbCtrl.getHoldingsDriver():
-        if 'moneydance' in globals():
-            from com.moneydance.apps.md.controller import FeatureModuleContext
-            from NbImporter import NbImporter
-
+        if nbCtrl.getHoldingsDriver():
             if nbCtrl.navigateToHoldingsDetails():
-                global moneydance  # type: FeatureModuleContext
+                global moneydance
                 importer = NbImporter(lookupWin, moneydance.currentAccountBook)
                 importer.obtainPrices(nbCtrl.getTitle(), nbCtrl.getHoldings())
                 lookupWin.enableCommitButton(importer.isModified())
-        else:
-            lookupWin.addText("Not started via Moneydance, close this window to clean up")
-            nbCtrl.showInFront()
+    else:
+        System.err.write("Not started via Moneydance, missing global variable.\n")
